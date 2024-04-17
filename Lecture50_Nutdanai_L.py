a10af3ca5708507c68f7563eedc8f9c33512d428
Nutdanai Likitpiriya <161489026+NutdanaiL@users.noreply.github.com>
2024-04-17 17:40:01 +0700

    Lec50

---
+++ Lecture50_Nutdanai_L.py
@@ -0,0 +1,13 @@
+def addNumber(x,y):
+    print(x+y)
+def minusNumber(x,y):
+    print(x-y)
+def multipliedNumber(x,y):
+    print(x*y)
+def devideNumber(x, y):
+    print(x/y)
+
+addNumber(5,8)
+minusNumber(5,8)
+multipliedNumber(5,8)
+devideNumber(5,8)
